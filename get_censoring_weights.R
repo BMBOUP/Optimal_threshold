1fc9db0049b10037807818366c2d6d74f2ec4a9a
Bassirou Mboup <45500173+BMBOUP@users.noreply.github.com>
2018-12-01 22:51:47 +0100

    Update get_censoring_weights.R

--- get_censoring_weights.R
+++ get_censoring_weights.R
@@ -1,19 +1,23 @@
 # {{{ inputs description :
-# time      :
-# event     :
+# time           : vector of observed failure times
+# event          : vector of indicator of status (0 for censoring, 1 for type of event)
 # timepoint : prediction time 
+# }}}
+# {{{ outputs description :
+# weights     : inverse of probability of censored weighting
+# }}}
 
 library(prodlim)
 get.censoring.weights <- function(timepoint,time,event){
   untreated <- time >= timepoint 
   censure <- time < timepoint & event==0 
   new.times <- time
-  Fitcens <- prodlim(Hist(time,event)~1,reverse=TRUE)
+  Fitcens <- prodlim::prodlim(Hist(time,event)~1,reverse=TRUE)
   new.times [untreated] <- timepoint
   ## increasing time 
   times_sorted <- sort(new.times,decreasing=FALSE,method='shell')
   order <- rank(new.times)
-  weights <- predict(Fitcens,times=times_sorted)[order]
+  weights <- prodlim::predict(Fitcens,times=times_sorted)[order]
   weights <- 1/weights
   weights[censure] <- 0 
   return(weights)
