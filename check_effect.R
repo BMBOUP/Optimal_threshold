6980271511b8b6a2eb1602a1aac2697a1703e142
Bassirou Mboup <45500173+BMBOUP@users.noreply.github.com>
2018-12-03 22:48:46 +0100

    Update check_effect.R

--- check_effect.R
+++ check_effect.R
@@ -1,4 +1,12 @@
-
+# {{{ inputs descriptions:
+# time       : the vector of observed tim-to-event
+# event      : 0=censored, 1=uncensored
+# treatment  : 0=untreated , 1= treated
+# Marker     : vector of biomarker value
+# }}}
+# {{{ Outputs :
+#       graphics
+# }}}
 
 Check_effect <- function(time,event,treatment,Marker){
     data <- cbind.data.frame(time=time,event=event,treatment=treatment,Marker=Marker)
