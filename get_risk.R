d83b2db69800b14fc7606495aa74d3a86d1bf05c
Bassirou Mboup <45500173+BMBOUP@users.noreply.github.com>
2018-12-04 01:07:33 +0100

    Update get_risk.R

--- get_risk.R
+++ get_risk.R
@@ -14,19 +14,19 @@
 # }}}
 # {{{ output description : a list of
 # threshold         : the optimal theshold of the biomarker (or cut-off)
+# confint           : the confidence intervale low and upper
 # Pneg              : the pourcentage of subjects who avoid or take the treatment à time t
 # risk_0            : P(D(t)=1|Treat=0,Marker)
 # risk_1            : P(D(t)=1|Treat=1,Marker)
 # }}}
 get_risk <- function(time,event,Treat,Marker,varying,timepoint)
 {
-  if (length(event)!=length(time) | length(Marker)!=length(time)|length(Treat)!=length(time) | length(event)!=length(time)){
-    stop("lengths of vector time, event,treatment and Marker have to be equal\n") }
+  if (length(event)!=length(time) | length(Marker)!=length(time)|length(Treat)!=length(time) | length(event)!=length(time))
+  {stop("lengths of vector time, event,treatment and Marker have to be equal\n") }
   if (missing(timepoint)){
     stop("Choose one time for horizon prediction \n") }
   
   data <- cbind.data.frame(time=time,event=event,Treat=Treat,Marker=Marker)
-  
   
   if(varying==FALSE || missing(varying)) {
     estim <- comp.risk(Event(time,event)~const(Treat)+const(Marker)+
@@ -38,9 +38,7 @@ get_risk <- function(time,event,Treat,Marker,varying,timepoint)
                                 n.sim=1000,
                                 model="logistic")
   }
-    
     if(varying==TRUE){
-      
       estim <- comp.risk(Event(time,event)~Treat+const(Marker)+
                                     const(Treat*Marker),
                                   cause=1,
@@ -50,17 +48,10 @@ get_risk <- function(time,event,Treat,Marker,varying,timepoint)
                                   n.sim=1000,
                                   model="logistic")
     }
-    
-    
-    
-
-  
   predittreat1 <- predict(estim,newdata=data.frame(Marker=sort(Marker),Treat=1),
                                    n.sim=1,times=timepoint)$P1
   predittreat0<- predict(estim,newdata=data.frame(Marker=sort(Marker),Treat=0),
                                   n.sim=1,times=timepoint)$P1
-  
-  
   Delta <- predittreat0-predittreat1
   Pneg <- length(Delta[Delta<0])/length(Delta)
   
@@ -71,9 +62,6 @@ get_risk <- function(time,event,Treat,Marker,varying,timepoint)
   
   result <- boot(data=data$Marker,statistic=f,R=999)
   ci <- boot.ci(result,conf=0.95, type="norm")
-  
-  paste("(", round(ci$normal[2],2),",",round(ci$normal[3],2),")")
-
   risk <- list(threshold=round(result$t0,2),
                  confint=paste("(", round(ci$normal[2],2),",",round(ci$normal[3],2),")"),
                     Pneg=(Pneg*100), 
