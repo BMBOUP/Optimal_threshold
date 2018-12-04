8b3a48f739964c9658111ad2500ba07e4273aeec
Bassirou Mboup <45500173+BMBOUP@users.noreply.github.com>
2018-12-04 02:29:23 +0100

    Update Marginal_effect_timepoint.R

--- Marginal_effect_timepoint.R
+++ Marginal_effect_timepoint.R
@@ -21,12 +21,12 @@ Marginal_effecttimepoint<-function(time,event,Treat,timepoint) {
     untreated <- time >= timepoint 
     censure <- time < timepoint & event==0 
     new.times <- time
-    Fitcens <- prodlim(Hist(time,event)~1,reverse=TRUE)
+    Fitcens <- prodlim::prodlim(Hist(time,event)~1,reverse=TRUE)
     new.times [untreated] <- timepoint
     ## increasing time 
     times_sorted <- sort(new.times,decreasing=FALSE,method='shell')
     order <- rank(new.times)
-    weights <- predict(Fitcens,times=times_sorted)[order]
+    weights <- prodlim::predict(Fitcens,times=times_sorted)[order]
     weights <- 1/weights
     weights[censure] <- 0 
     return(weights)
