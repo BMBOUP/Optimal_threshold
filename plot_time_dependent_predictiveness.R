410ed538d0ad03b8b773b959927a3851654200c5
Bassirou Mboup <45500173+BMBOUP@users.noreply.github.com>
2018-12-04 01:27:54 +0100

    Update plot_time_dependent_predictiveness.R

--- plot_time_dependent_predictiveness.R
+++ plot_time_dependent_predictiveness.R
@@ -1,20 +1,19 @@
 
 # {{{ input description :
-
-# fitted         : an object of get_risk
+# Object         : an object of get_risk
 # timepoint      : the prediction time
-# Marker         : vector of continuous biomarker values
+# Marker         : vector biomarker values
 
 # }}}
 
-plotime_predictiveness_curve <- function(fitted,timepoint,Marker){
+plotime_predictiveness_curve <- function(Object,timepoint,Marker){
   xlim <- c(0,100)
   breaks = seq(xlim[1], xlim[2], length.out = 5)
-  k <- length(fitt$risk_0)
+  k <- length(Object$risk_0)
   x1<- sort(Marker)
   y1 <- (((1:k)*100)/k)
-  z1 <- fitt$risk_0
-  z2 <- fitt$risk_1
+  z1 <- Object$risk_0
+  z2 <- Object$risk_1
   plot(y1,z1,type='l',col="red",xlab="(%)population \n marker Value",ylab="Risk given marker (%)",
        main=paste("Time dependent marker-by-treatment \n predictiveness curve at times t=",timepoint,"years"),ylim=c(0,1),axes=FALSE)
   lines(y1,z2,type='l',col="blue",lwd=2)
